88668bf8c3fd300df6922e16ac51da20d7b061ed
JessycaAmaral <kinhamaagnata@gmail.com>
2023-11-10 14:32:50 -0300

    Sistema de Celular POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "622102102", modelo: "N95", imei: "1025418d123654", memoria: 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("IPhone:");
+Smartphone iphone = new Iphone(numero: "1541542323", modelo: "11 x", imei: "102141257891523", memoria: 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
